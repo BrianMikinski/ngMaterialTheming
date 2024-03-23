896e8162680cd3de2a24e6f8a14da1a96d95135d
Brian Mikinski <brian.mikinski@gmail.com>
2024-03-22 23:38:56 -0500

    updates

--- ngMaterialTheming/src/app/app.component.ts
+++ ngMaterialTheming/src/app/app.component.ts
@@ -1,21 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, 
+  imports: [CommonModule,
     RouterOutlet,
-    // MatButtonModule,
-    // MatIconModule,
-    // MatDividerModule,
-    // MatCheckboxModule,
-  
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatCheckboxModule,
+
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ngMaterialTheming';
+
+  toggleTheme() {
+    // add light-theme or dark-theme class to body
+    if (document.body.classList.contains('dark-theme')) {
+      document.body.classList.remove('dark-theme');
+      document.body.classList.add('light-theme');
+    } else {
+      document.body.classList.remove('light-theme');
+      document.body.classList.add('dark-theme');
+    }
+  }
 }
